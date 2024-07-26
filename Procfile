a01d44829aa8299524233484aa82cc1b84ad320e
thanujakmt <thanujakmsdr@gmail.com>
2024-07-26 11:56:58 +0530

    test

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn previewcardproj.wsgi
--- previewcardproj/settings.py
+++ previewcardproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5batx@na$@jp5#_rxxe=_9p^$&i3k^a*x1@oh3&88#z1s(gv=y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,9 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = 'previewcardapp/static/'
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'previewcardapp/static')]
+
 STATIC_URL = 'previewcardapp/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'previewcardapp/static')]
+
+STATIC_DIRS = [os.path.join(BASE_DIR, 'previewcardapp/static')]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
